5c78db4c8356c94069d7b3e93e669d4ded521ffe
cy.preuss <cy.preuss@localhost>
2009-11-15 18:29:15 +0100

    added VehicleFactoryArray class added test implementation inside NetCtfVehicleFactory

---
+++ Source/EduNetGames/Tutorial_07/NetCtfVehicleFactory.cpp
@@ -0,0 +1,91 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2009, Jan Fietz, Cyrus Preuss
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, 
+// are permitted provided that the following conditions are met:
+// 
+// * Redistributions of source code must retain the above copyright notice, 
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice, 
+//   this list of conditions and the following disclaimer in the documentation 
+//   and/or other materials provided with the distribution.
+// * Neither the name of EduNetGames nor the names of its contributors
+//   may be used to endorse or promote products derived from this software
+//   without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+// IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
+// ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+// EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//-----------------------------------------------------------------------------
+
+#include "NetCtfVehicleFactory.h"
+#include "NetCtfVehicles.h"
+
+//-----------------------------------------------------------------------------
+class NetCtfBaseVehicleFactory : public OpenSteer::TVehicleFactory<TNetCtfBaseVehicle>
+{
+	ET_DECLARE_BASE( OpenSteer::TVehicleFactory<TNetCtfBaseVehicle> );
+public:
+	NetCtfBaseVehicleFactory(){};
+	virtual ~NetCtfBaseVehicleFactory(){};
+};
+
+//-----------------------------------------------------------------------------
+class NetCtfSeekerVehicleFactory : public OpenSteer::TVehicleFactory<TNetCtfSeekerVehicle>
+{
+	ET_DECLARE_BASE( OpenSteer::TVehicleFactory<TNetCtfSeekerVehicle> );
+public:
+	NetCtfSeekerVehicleFactory(){};
+	virtual ~NetCtfSeekerVehicleFactory(){};
+};
+
+//-----------------------------------------------------------------------------
+class NetCtfEnemyVehicleFactory : public OpenSteer::TVehicleFactory<TNetCtfEnemyVehicle>
+{
+	ET_DECLARE_BASE( OpenSteer::TVehicleFactory<TNetCtfSeekerVehicle> );
+public:
+	NetCtfEnemyVehicleFactory(){};
+	virtual ~NetCtfEnemyVehicleFactory(){};
+};
+
+//-----------------------------------------------------------------------------
+// now 3 global vehicle factories
+NetCtfBaseVehicleFactory gNetCtfBaseVehicleFactory;
+NetCtfSeekerVehicleFactory gNetCtfSeekerVehicleFactory;
+NetCtfEnemyVehicleFactory gNetCtfEnemyVehicleFactory;
+
+//-----------------------------------------------------------------------------
+NetCtfVehicleFactory::NetCtfVehicleFactory()
+{
+	this->addVehicleFactory( &gNetCtfBaseVehicleFactory );
+	this->addVehicleFactory( &gNetCtfSeekerVehicleFactory );
+	this->addVehicleFactory( &gNetCtfEnemyVehicleFactory );
+
+	// test the 3 vehicle classes
+	OpenSteer::AbstractVehicle* pkVehicle = NULL;
+	pkVehicle = this->createVehicle( ET_CID_CTF_BASE_VEHICLE, NULL );
+	this->destroyVehicle( pkVehicle );
+	pkVehicle = this->createVehicle( ET_CID_CTF_ENEMY_VEHICLE, NULL );
+	this->destroyVehicle( pkVehicle );
+	pkVehicle = this->createVehicle( ET_CID_CTF_SEEKER_VEHICLE, NULL );
+	this->destroyVehicle( pkVehicle );
+}
+
+//-----------------------------------------------------------------------------
+NetCtfVehicleFactory::~NetCtfVehicleFactory()
+{
+
+}
+
+//-----------------------------------------------------------------------------
+// now 1 global vehicle factory for unit tests
+NetCtfVehicleFactory gNetCtfVehicleFactory;
+
