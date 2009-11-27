7605652098e311553cc7e9b432a529a9450486bf
cy.preuss <cy.preuss@localhost>
2009-11-27 21:27:25 +0100

    bug fixing

--- Source/src/EduNetApplication/EduNetGames.cpp
+++ Source/src/EduNetApplication/EduNetGames.cpp
@@ -1142,7 +1142,8 @@ OpenSteer::OpenSteerDemo::initializeGraphics (int argc, char **argv)
 	// (center window on screen with size equal to "ws" times screen size)
 	const int sw = glutGet(GLUT_SCREEN_WIDTH);
 	const int sh = glutGet(GLUT_SCREEN_HEIGHT);
-	const float ws = 0.8f; // window_size / screen_size
+	const float ws = 0.5f; // window_size / screen_size
+//	const float ws = 0.8f; // window_size / screen_size
 	const int ww = (int) (sw * ws);
 	const int wh = (int) (sh * ws);
 	glutInitWindowPosition ((int) (sw * (1-ws)/2), (int) (sh * (1-ws)/2));
