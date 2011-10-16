54eb07558e20b7a0f7f60a664a9f2d1190a4db3b
janfietz <janfietz@localhost>
2011-10-16 10:42:06 +0200

    -fixed build -disabled some warnigns on msvc builds

--- Source/src/EduNetCommon/EduNetOptions.cpp
+++ Source/src/EduNetCommon/EduNetOptions.cpp
@@ -36,8 +36,18 @@
 // To include EXIT_SUCCESS
 #include <cstdlib>
 
+#if defined (_MSC_VER)
+	#pragma warning(push)
+	#pragma warning(disable:4251)
+	#pragma warning(disable:4275)
+#endif
+
 #include <boost/program_options/parsers.hpp>
 #include <boost/program_options/errors.hpp>
+
+#if defined (_MSC_VER)
+	#pragma warning(pop)
+#endif
 
 using namespace boost;
 
--- Source/src/EduNetCommon/EduNetOptions.h
+++ Source/src/EduNetCommon/EduNetOptions.h
@@ -30,8 +30,20 @@
 //-----------------------------------------------------------------------------
 
 #include "EduNetCore/EduNetCore.h"
+
+#if defined (_MSC_VER)
+	#pragma warning(push)
+	#pragma warning(disable:4251)
+	#pragma warning(disable:4275)
+#endif
+
 #include <boost/program_options/variables_map.hpp>
 #include <boost/program_options/options_description.hpp>
+
+#if defined (_MSC_VER)
+	#pragma warning(pop)
+#endif
+
 namespace EduNet	{
 
 	//-------------------------------------------------------------------------
